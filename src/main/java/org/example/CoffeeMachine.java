2908d5d2a7e0cd152bc64fef1c0f596eb7d7e0f2
viktorialu <vlukidi@stvdunice.net>
2024-07-22 09:37:13 +0300

    taskCoffeMachine2

--- src/main/java/org/example/CoffeeMachine.java
+++ src/main/java/org/example/CoffeeMachine.java
@@ -1,106 +1,152 @@
 package org.example;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import static org.example.CoffeeMachine.milkLevel;
+import static org.example.CoffeeMachine.waterLevel;
 
 public class CoffeeMachine {
-    private boolean isOn;
-    private int water;
-    private int coffee;
-    private int milk;
-    private final int MAX_WATER_LEVEL = 2000;
-    private final int MAX_COFFE_LEVEL = 500;
-    private final int MAX_MILK_LEVEL = 1000;
 
-    public CoffeeMachine() {
-        this.isOn = false;
-        this.water = 0;
-        this.coffee = 0;
-        this.milk = 0;
+    private static final int ESPRESSO_WATER = 50;
+    private static final int ESPRESSO_MILK = 0;
+
+    private static final int CAPPUCCINO_WATER = 30;
+    private static final int CAPPUCCINO_MILK = 20;
+
+    static int waterLevel;
+    static int milkLevel;
+
+    private int maxWaterLevel;
+    private int maxMilkLevel;
+
+    private int drinksPrepared;
+    private static final int CLEANING_THRESHOLD = 10;
+
+    public CoffeeMachine(int maxWaterLevel, int maxMilkLevel) {
+        this.maxWaterLevel = maxWaterLevel;
+        this.maxMilkLevel = maxMilkLevel;
+        waterLevel = maxWaterLevel;
+        milkLevel = maxMilkLevel;
+        drinksPrepared = 0;
     }
-    public class CoffeeMachineException extends Exception {
+
+    public boolean prepareEspresso() {
+        if (waterLevel < ESPRESSO_WATER) {
+            System.out.println("Недостаточно воды для эсперессо!");
+            return false;
+        }
+        waterLevel -= ESPRESSO_WATER;
+        drinksPrepared++;
+        checkIfNeedsCleaning();
+        return true;
+    }
+
+    public boolean prepareCappuccino() {
+        if (waterLevel < CAPPUCCINO_WATER) {
+            System.out.println("Не достаточно воды для каппучино!");
+            return false;
+        }
+        if (milkLevel < CAPPUCCINO_MILK) {
+            System.out.println("Не достаточно молока для каппучино!");
+            return false;
+        }
+        waterLevel -= CAPPUCCINO_WATER;
+        milkLevel -= CAPPUCCINO_MILK;
+        drinksPrepared++;
+        checkIfNeedsCleaning();
+        return true;
+    }
+
+    public void refillWater() {
+        waterLevel = maxWaterLevel;
+        System.out.println("Долив воды.");
+    }
+
+    public void refillMilk() {
+        milkLevel = maxMilkLevel;
+        System.out.println("Долив молока.");
+    }
+
+    private void checkIfNeedsCleaning() {
+        if (drinksPrepared >= CLEANING_THRESHOLD) {
+            cleanMachine();
+            drinksPrepared = 0;
+        }
+    }
+
+    private void cleanMachine() {
+        System.out.println("Очистка кофемашины...");
+    }
+
+    public int getWaterLevel() {
+        return waterLevel;
+    }
+
+    public int getMilkLevel() {
+        return milkLevel;
+    }
+
+    public static void main(String[] args) {
+        CoffeeMachine coffeeMachine = new CoffeeMachine(1000, 500);
+
+        coffeeMachine.prepareEspresso();
+        coffeeMachine.prepareCappuccino();
+        coffeeMachine.refillWater();
+        coffeeMachine.refillMilk();
+    }
+}
+
+enum DrinkType {
+    ESPRESSO,
+    CAPPUCCINO;
+
+    static class CoffeeMachineException extends Exception {
         public CoffeeMachineException(String message) {
             super(message);
         }
     }
-    public void turnOn() {
-        isOn = true;
+
+    public void makeDrink(DrinkType type) throws CoffeeMachineException {
     }
 
-    public void turnOff() {
-        isOn = false;
-    }
-
-    public void addWater(int amount) throws CoffeeMachineException {
-        if (water + amount > MAX_WATER) {
-            throw new CoffeeMachineException("Too much water!");
+    public void makeCoffee(DrinkType type) throws CoffeeMachineException {
+        switch (type) {
+            case ESPRESSO:
+                checkWaterLevel(50, "Недостаточно воды для эспрессо!");
+                makeEspresso();
+                break;
+            case CAPPUCCINO:
+                checkIngredients(30, 20, "Недостаточно ингредиентов для каппучино!");
+                makeCappuccino();
+                break;
+            default:
+                throw new CoffeeMachineException("Неизвестный напиток!");
         }
-        water += amount;
     }
 
-    public void addCoffee(int amount) throws CoffeeMachineException {
-        if (coffee + amount > MAX_COFFEE) {
-            throw new CoffeeMachineException("Too much coffee!");
+    private void makeCappuccino() {
+    }
+
+    private void makeEspresso() {
+    }
+
+    private static void checkWaterLevel(int requiredWater, String errorMessage) throws CoffeeMachineException {
+        if (waterLevel < requiredWater) {
+            throw new CoffeeMachineException(errorMessage);
         }
-        coffee += amount;
     }
 
-    public void addMilk(int amount) throws CoffeeMachineException {
-        if (milk + amount > MAX_MILK) {
-            throw new CoffeeMachineException("Too much milk!");
+    private static void checkIngredients(int requiredWater, int requiredMilk, String errorMessage) throws CoffeeMachineException {
+        if (waterLevel < requiredWater || milkLevel < requiredMilk) {
+            throw new CoffeeMachineException(errorMessage);
         }
-        milk += amount;
     }
 
-    public int checkWaterLevel() {
-        return water;
+    public String getRecipe(DrinkType type) {
+        switch (type) {
+            case ESPRESSO:
+                return "Эспрессо: 50мл воды";
+            case CAPPUCCINO:
+                return "Капучино: 30мл воды, 20мл молока";
+            default:
+                return "Неизвестный рецепт";
+        }
     }
-
-    public int checkMilkLevel() {
-        return milk;
-    }
-
-    public boolean hasCoffee() {
-        return coffee > 0;
-    }
-
-
-    public class Main {
-        public static void main(String[] args) {
-            CoffeeMachine coffeeMachine = new CoffeeMachine();
-            Scanner scanner = new Scanner(System.in);
-            boolean exit = false;
-
-            while (!exit) {
-                System.out.println("Выбрать опцию");
-                System.out.println("1. Включить");
-                System.out.println("2. Выключить");
-                System.out.println("3. Добавить воды");
-                System.out.println("4. Добавить молока");
-                System.out.println("5. Добавить кофе");
-                System.out.println("6. Проверить уворень воды");
-                System.out.println("7. Проверить уровень молока");
-                System.out.println("8. Проверить уровень кофе");
-                System.out.println("9. Проверить уровень загрязнения");
-                System.out.println("10. Очистка");
-                System.out.println("11. Сделать эсспресо");
-                System.out.println("12. Сделать каппучино");
-                System.out.println("13. Выход");
-                int option = scanner.nextInt();
-
-                try {
-                    switch (option) {
-                        case 1:
-                            coffeeMachine.turnOn();
-                            Logger.log("Включить");
-                            break;
-                        case 2:
-                            coffeeMachine.turnOff();
-                            Logger.log("Выключить");
-                            break;
-
-                        scanner.close();
-                    }
-                } finally {
-
-                }
+}
